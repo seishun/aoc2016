3d90367a51488a3afc0456d789c102d39fbc90c6
Nikolai Vavilov <vvnicholas@gmail.com>
2016-12-03 15:20:29 +0200

    Add day3 part2

--- day3.hs
+++ day3.hs
@@ -1,5 +1,14 @@
-isTriangle :: [Int] -> Bool
-isTriangle [a, b, c] = a + b > c && a + c > b && b + c > a
+import Data.List
+
+isTriangle :: Int -> Int -> Int -> Bool
+isTriangle a b c = a + b > c && a + c > b && b + c > a
+
+countTriangles :: [Int] -> Int
+countTriangles [] = 0
+countTriangles (a:b:c:xs) = countTriangles xs + fromEnum (isTriangle a b c)
 
 part1 :: String -> Int
-part1 = length . filter (isTriangle . map read . words) . lines
+part1 = countTriangles . map read . words
+
+part2 :: String -> Int
+part2 = countTriangles . map read . concat . transpose . map words . lines
