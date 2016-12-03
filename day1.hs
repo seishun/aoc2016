a7aa4e691d5781e4c418db596b6b7ffb052cff2a
Nikolai Vavilov <vvnicholas@gmail.com>
2016-12-03 13:34:16 +0200

    Add day1 part2

--- day1.hs
+++ day1.hs
@@ -1,4 +1,6 @@
 import Data.Complex
+import Data.List
+import Data.Maybe
 
 direction :: Char -> Complex Float
 direction 'L' = 0 :+ 1
@@ -18,10 +20,22 @@ advance (dir, pos) (turn, steps) =
       pos' = pos + dir' * (steps :+ 0)
   in (dir', pos')
 
-day1 :: String -> Int
-day1 input =
+part1 :: String -> Int
+part1 input =
   let parts = parse input
       (_, (x :+ y)) = foldl advance (0 :+ 1, 0 :+ 0) parts
   in grid x + grid y
   where grid = round . abs
 
+firstTwice :: (Complex Float, Complex Float) -> [(Complex Float, Float)] -> [Complex Float] -> Complex Float
+firstTwice (dir, pos) (x:xs) prev =
+  let (turn, steps) = x
+      (dir':_, poss@(pos':_)) = unzip $ map (advance (dir, pos)) $ map (\s -> (turn, s)) $ reverse [1..steps]
+  in fromMaybe (firstTwice (dir', pos') xs (poss ++ prev)) (listToMaybe $ intersect poss prev)
+
+part2 :: String -> Int
+part2 input =
+  let parts = parse input
+      (x :+ y) = firstTwice (0 :+ 1, 0 :+ 0) parts []
+  in grid x + grid y
+  where grid = round . abs
