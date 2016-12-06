a8d4aa4b542f29092ccf88218bb52282ba4548bc
Nikolai Vavilov <vvnicholas@gmail.com>
2016-12-06 20:22:10 +0200

    Add day6 part2

--- day6.hs
+++ day6.hs
@@ -2,3 +2,6 @@ import Data.List
 
 part1 :: String -> String
 part1 = map (head . head . reverse . sortOn length . group . sort) . transpose . words
+
+part2 :: String -> String
+part2 = map (head . head . sortOn length . group . sort) . transpose . words
