712355931da699d1c2d0ce3345ab009ff8d8bf95
Nikolai Vavilov <vvnicholas@gmail.com>
2016-12-08 12:59:03 +0200

    Add day8 part1

---
+++ day8.hs
@@ -0,0 +1,31 @@
+import Data.List
+
+rect :: Int -> Int -> [String] -> [String]
+rect a b screen = map (\row -> replicate a '#' ++ drop a row) (take b screen) ++ drop b screen
+
+shift :: Int -> String -> String
+shift a pixels = drop n pixels ++ take n pixels
+  where n = length pixels - a
+
+rotateRow :: Int -> Int -> [String] -> [String]
+rotateRow a b screen = take a screen ++ [shift b $ screen !! a] ++ tail (drop a screen)
+
+rotateColumn :: Int -> Int -> [String] -> [String]
+rotateColumn a b = transpose . rotateRow a b . transpose
+
+parse :: [String] -> [String] -> [String]
+parse ["rect", a] =
+  let [(a', 'x':b)] = reads a
+      b' = read b
+  in rect a' b'
+parse ["rotate", "row", 'y':'=':a, "by", b] =
+  let a' = read a
+      b' = read b
+  in rotateRow a' b'
+parse ["rotate", "column", 'x':'=':a, "by", b] =
+  let a' = read a
+      b' = read b
+  in rotateColumn a' b'
+
+part1 :: String -> Int
+part1 = length . filter (== '#') . concat . foldl (flip parse) (replicate 6 $ replicate 50 '.') . map words . lines
