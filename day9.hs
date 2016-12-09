24f46c0bc10cfe06c8112c9778744832e0f07a28
Nikolai Vavilov <vvnicholas@gmail.com>
2016-12-09 17:48:00 +0200

    Add day9 part2

--- day9.hs
+++ day9.hs
@@ -6,5 +6,16 @@ decompress ('(':xs) =
 decompress (x:xs) = [x] ++ decompress xs
 decompress [] = []
 
+decompress_v2 :: String -> Int
+decompress_v2 ('(':xs) =
+  let [(chars, 'x':xs')] = reads xs
+      [(times, ')':xs'')] = reads xs'
+  in times * (decompress_v2 $ take chars xs'') + decompress_v2 (drop chars xs'')
+decompress_v2 (x:xs) = 1 + decompress_v2 xs
+decompress_v2 [] = 0
+
 part1 :: String -> Int
 part1 = length . decompress
+
+part2 :: String -> Int
+part2 = decompress_v2
