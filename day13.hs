86a7fd81433fed3c47cf50bd41d7b22757365824
Nikolai Vavilov <vvnicholas@gmail.com>
2016-12-13 17:54:31 +0200

    Add day13 part2

--- day13.hs
+++ day13.hs
@@ -1,3 +1,4 @@
+import Control.Monad
 import Data.Bits
 import Data.Set (Set)
 import qualified Data.Set as Set
@@ -11,9 +12,11 @@ isOpenSpace favorite (x, y) =
   in bits `mod` 2 == 0
 
 neighbors :: Int -> Coordinate -> [Coordinate]
-neighbors favorite (x, y) =
-  let directions = [(x-1,y),(x,y-1),(x,y+1),(x+1,y)]
-  in filter (isOpenSpace favorite) directions
+neighbors favorite (x, y) = do
+  (x', y') <- [(x-1,y),(x,y-1),(x,y+1),(x+1,y)]
+  guard $ x' >= 0 && y' >= 0
+  guard $ isOpenSpace favorite (x', y')
+  return (x', y')
 
 dijkstra :: Int -> Coordinate -> Set Coordinate -> Set Coordinate -> Int
 dijkstra favorite target visited unvisited =
@@ -23,5 +26,16 @@ dijkstra favorite target visited unvisited =
            visited' = Set.union visited unvisited
   in 1 + dijkstra favorite target visited' unvisited'
 
+dijkstra' :: Int -> Int -> Set Coordinate -> Set Coordinate -> Set Coordinate
+dijkstra' favorite steps visited unvisited =
+  let unvisited' = Set.filter (`Set.notMember` visited) $ Set.fromList $ concatMap (neighbors favorite) unvisited
+      visited' = Set.union visited unvisited
+  in if steps == 0
+  then visited'
+  else dijkstra' favorite (steps - 1) visited' unvisited'
+
 part1 :: Int -> Int
 part1 input = dijkstra input (31,39) Set.empty (Set.singleton (1,1))
+
+part2 :: Int -> Int
+part2 input = Set.size $ dijkstra' input 50 Set.empty (Set.singleton (1,1))
