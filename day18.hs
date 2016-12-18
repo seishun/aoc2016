849c699b941d6a9492f521c8003882c76b301103
Nikolai Vavilov <vvnicholas@gmail.com>
2016-12-18 18:19:23 +0200

    Add day18 part2

--- day18.hs
+++ day18.hs
@@ -11,3 +11,6 @@ nextRow previous =
 
 part1 :: String -> Int
 part1 = length . filter (== '.') . concat . take 40 . iterate nextRow
+
+part2 :: String -> Int
+part2 = length . filter (== '.') . concat . take 400000 . iterate nextRow
