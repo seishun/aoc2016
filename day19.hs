880e6337a2d4fbbd9069dc2ae0ae3486e5a2f5d2
Nikolai Vavilov <vvnicholas@gmail.com>
2016-12-19 16:12:31 +0200

    Add day19 part2

--- day19.hs
+++ day19.hs
@@ -1,4 +1,4 @@
-import Prelude hiding (drop, null)
+import Prelude hiding (drop, length, null)
 import Data.Sequence
 
 lastElf :: Seq Int -> Int
@@ -8,5 +8,15 @@ lastElf elves =
   then elf
   else lastElf $ drop 1 xs |> elf
 
+lastElf' :: Seq Int -> Int
+lastElf' elves =
+  let (elf :< xs) = viewl elves
+  in if null xs
+  then elf
+  else lastElf' $ deleteAt (length elves `div` 2 - 1) xs |> elf
+
 part1 :: Int -> Int
 part1 number = lastElf $ fromList [1..number]
+
+part2 :: Int -> Int
+part2 number = lastElf' $ fromList [1..number]
