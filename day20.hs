e43a80c677755fd6b65de4a731f5ada62f76ace1
Nikolai Vavilov <vvnicholas@gmail.com>
2016-12-20 17:19:19 +0200

    Add day20 part2

--- day20.hs
+++ day20.hs
@@ -6,6 +6,12 @@ lowest candidate ((lo, hi):xs) =
   if lo > candidate then candidate
   else lowest (max candidate (hi + 1)) xs
 
+total :: Int -> [(Int, Int)] -> Int
+total candidate [] = 4294967295 - candidate + 1
+total candidate ((lo, hi):xs) =
+  if lo > candidate then lo - candidate + total (hi + 1) xs
+  else total (max candidate (hi + 1)) xs
+
 parse :: String -> (Int, Int)
 parse line =
   let [(lo, '-':hi)] = reads line
@@ -13,3 +19,6 @@ parse line =
 
 part1 :: String -> Int
 part1 = lowest 0 . sort . map parse . lines
+
+part2 :: String -> Int
+part2 = total 0 . sort . map parse . lines
