07e4ce1a457a5fd688fb13c615c96cdc7e7d4111
Nikolai Vavilov <vvnicholas@gmail.com>
2016-12-24 19:20:07 +0200

    Add day23 part2

--- day23.hs
+++ day23.hs
@@ -1,3 +1,4 @@
+import Data.Foldable (toList)
 import Data.Map (Map, (!))
 import qualified Data.Map as Map
 import Data.Sequence as Seq
@@ -9,7 +10,12 @@ execute :: Int -> Map Char Int -> Seq Instruction -> Int
 execute ip registers instructions = case instructions !? ip of
   Nothing -> registers ! 'a'
   Just instruction -> case instruction of
-    Cpy x (Register y) -> execute (ip + 1) (Map.insert y (value x) registers) instructions
+    Cpy x (Register y) -> case toList $ Seq.take 6 $ Seq.drop ip instructions of
+      [Cpy b1 (Register c1), Inc a1, Dec c2, Jnz (Register c3) (Literal (-2)), Dec d1, Jnz (Register d2) (Literal (-5))] ->
+        if c1 == c2 && c2 == c3 && d1 == d2
+        then execute (ip + 6) (Map.insert d1 0 $ Map.insert c1 0 $ Map.adjust (+ value b1 * registers ! d1) a1 registers) instructions
+        else execute (ip + 1) (Map.insert y (value x) registers) instructions
+      _ -> execute (ip + 1) (Map.insert y (value x) registers) instructions
     Cpy _ (Literal _) -> execute (ip + 1) registers instructions
     Inc x -> execute (ip + 1) (Map.adjust succ x registers) instructions
     Dec x -> execute (ip + 1) (Map.adjust pred x registers) instructions
@@ -41,3 +47,6 @@ parse ["tgl", [x]] = Tgl x
 
 part1 :: String -> Int
 part1 = execute 0 (Map.fromList (('a', 7) : [(c, 0) | c <- "bcd"])) . Seq.fromList . map (parse . words) . lines
+
+part2 :: String -> Int
+part2 = execute 0 (Map.fromList (('a', 12) : [(c, 0) | c <- "bcd"])) . Seq.fromList . map (parse . words) . lines
